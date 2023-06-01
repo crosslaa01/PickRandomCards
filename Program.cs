6d7cfaa875117ba0872acf796191b11bdefd1b89
Antony Crossley <crossley.94@hotmail.com>
2023-06-01 11:25:10 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PickRandomCards;
+
+Console.Write("Enter the number of cards to pick: ");
+string line = Console.ReadLine();
+
+
+if (int.TryParse(line, out int numberOfCards))
+{
+    foreach (string card in CardPicker.PickSomeCards(numberOfCards))
+    {
+        Console.WriteLine(card);
+    }
+}
+else
+{
+    Console.Write("Invalid number entered. ");
+}
+
+
